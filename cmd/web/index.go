9b662a963bfe1e808d63ba951e33a9193cb9435b
Mano Sriram <mano.sriram0@gmail.com>
2021-03-19 22:36:51 +0530

    Add Gorilla Mux.

--- cmd/web/index.go
+++ cmd/web/index.go
@@ -1,12 +1,39 @@
 package main
 
 import (
-	"github.com/manosriram/go-api/pkg/handlers"
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/home", handlers.Home);
+type Author struct {
+	name string `json:"name"`
+}
 
-	_ = http.ListenAndServe(":8000", nil);
+type Book struct {
+	id     string  `json:"id"`
+	isbn   string  `json:"isbn"`
+	author *Author `json:"author"`
+}
+
+var Books []Book
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	payload, _ := json.Marshal(Books)
+	fmt.Println(payload)
+	w.Write(payload)
+}
+
+func main() {
+	r := mux.NewRouter()
+	Books = append(Books, Book{id: "1", isbn: "1234", author: &Author{name: "Mano"}})
+	Books = append(Books, Book{id: "2", isbn: "5234", author: &Author{name: "Sriram"}})
+	Books = append(Books, Book{id: "3", isbn: "12313", author: &Author{name: "Mano Sriram"}})
+
+	r.HandleFunc("/", getBooks).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,16 +1,68 @@
 package render
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
 
+var functions = template.FuncMap{}
+
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl);
-	err := parsedTemplate.Execute(w, nil);
+
+	tc, err := CreateTemplateCache(w, tmpl)
 	if err != nil {
-		fmt.Println(err);
-		return;
+		log.Fatal()
 	}
+
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal()
+	}
+
+	buf := new(bytes.Buffer)
+	_ = t.Execute(buf, nil)
+	_, err = buf.WriteTo(w)
+	if err != nil {
+		fmt.Println("Error writing template to browser: ", err)
+	}
+
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err = parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func CreateTemplateCache(w http.ResponseWriter, tmpl string) (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return cache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		fmt.Println("Page is currently: ", name)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+
+		if err != nil {
+			return cache, err
+		}
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return cache, err
+			}
+		}
+		cache[name] = ts
+	}
+	return cache, nil
 }
